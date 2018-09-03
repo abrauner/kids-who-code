446ff27ab04e8601dc6dd352b6c2c3c2e37f606e
Laura Luiz <laura.luiz@commercetools.de>
2018-09-03 11:53:40 +0200

    Add easy implementation of hot potato

---
+++ Calliope/MakeCode/HotPotatoEasy.js
@@ -0,0 +1,36 @@
+// Implementation for Calliope of the game "hot potato".
+// Level: easy
+//
+// Game mechanics:
+// When button A is pressed, it will start a countdown,
+// during which time the participants have to pass the
+// Calliope around to each other until the time expires.
+// The participant that holds the Calliope when this
+// happens, loses.
+//
+// Functionality:
+// - During the countdown, it displays a constant
+// blinking green light and emits a beeping.
+// - When the expiration time arrives, it will dusplay
+// a red light and a skull icon and it will emit a
+// high-pitched beeping sound 5 times. 
+// - The expiration time is set randomly between 5 and
+// 25 seconds.
+
+let counter = 0
+input.onButtonPressed(Button.A, () => {
+    counter = 5 + Math.random(21)
+    while (counter > 0) {
+        basic.setLedColor(Colors.Green)
+        music.playTone(247, music.beat(BeatFraction.Quarter))
+        basic.setLedColor(0)
+        basic.pause(1000)
+        counter += -1
+    }
+    basic.setLedColor(Colors.Red)
+    basic.showIcon(IconNames.Skull)
+    for (let i = 0; i < 5; i++) {
+        music.playTone(880, music.beat(BeatFraction.Double))
+        music.playTone(784, music.beat(BeatFraction.Double))
+    }
+})
