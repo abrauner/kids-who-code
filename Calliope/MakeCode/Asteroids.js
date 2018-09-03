b941cc742b508a020927baf62cf8c4815e54dc11
Laura Luiz <laura.luiz@commercetools.de>
2018-09-03 20:58:52 +0200

    Asteroids: Fix game over case

--- Calliope/MakeCode/Asteroids.js
+++ Calliope/MakeCode/Asteroids.js
@@ -1,22 +1,22 @@
 let asteroids: game.LedSprite[] = []
 let spaceship: game.LedSprite = null
-input.onButtonPressed(Button.A, () => {
-    spaceship.move(-1)
-})
 input.onButtonPressed(Button.B, () => {
     spaceship.move(1)
 })
+input.onButtonPressed(Button.A, () => {
+    spaceship.move(-1)
+})
 spaceship = game.createSprite(2, 4)
-basic.pause(1000)
 while (true) {
     basic.pause(500)
     asteroids.push(game.createSprite(Math.random(5), 0))
     for (let asteroid of asteroids) {
-        if (asteroid.get(LedSpriteProperty.X) == spaceship.get(LedSpriteProperty.X) && asteroid.get(LedSpriteProperty.Y) == 3) {
+        if (asteroid.isTouching(spaceship)) {
+            music.beginMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Once)
             game.gameOver()
         } else if (asteroid.get(LedSpriteProperty.Y) == 4) {
             asteroid.delete()
-            game.setScore(game.score() + 10)
+            game.setScore(game.score() + 1)
         } else {
             asteroid.change(LedSpriteProperty.Y, 1)
         }
